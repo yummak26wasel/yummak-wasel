55018cebba9170a9f1b9818890b29d2def34d989
yummak26wasel <dhifvmy@gmail.com>
2026-08-25 01:49:30 +0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 /* ليمك واصل — Service Worker */
-const CACHE='leemak-v7';
+const CACHE='leemak-v8';
 const SHELL=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
